eecacdf53fdcbc482b2bf0da4ff0ccb69477a71d
Chris <chagmaier@oxy.edu>
2018-05-06 13:35:14 -0700

    eskeeeetit

--- app/src/main/java/com/example/chris/beerme/BeerResultList.java
+++ app/src/main/java/com/example/chris/beerme/BeerResultList.java
@@ -115,7 +115,7 @@ public class BeerResultList extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say a Beer Name!");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
--- app/src/main/java/com/example/chris/beerme/SearchResultsActivity.java
+++ app/src/main/java/com/example/chris/beerme/SearchResultsActivity.java
@@ -150,7 +150,7 @@ public class SearchResultsActivity extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say a Beer Name!");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
